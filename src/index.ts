2d68906926712341401f181f25d55da8760418cc
2nofa11 <agsh0505@gmail.com>
2026-03-29 18:15:29 +0900

    chore: remove UseSuspenseInfiniteQueryOptions from public exports
    Aligns with the React reference implementation which does not export this type.

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,6 @@ export { useTransport, provideTransport } from "./use-transport";
 export { useQuery, useSuspenseQuery } from "./use-query";
 export type { UseQueryOptions } from "./use-query";
 export { useInfiniteQuery, useSuspenseInfiniteQuery } from "./use-infinite-query";
-export type {
-  UseInfiniteQueryOptions,
-  UseSuspenseInfiniteQueryOptions,
-} from "./use-infinite-query";
+export type { UseInfiniteQueryOptions } from "./use-infinite-query";
 export { useMutation } from "./use-mutation";
 export type { UseMutationOptions } from "./use-mutation";
